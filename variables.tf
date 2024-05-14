2d5268bb588374a64f11b0b75d97a5aebe1464cd
Simon Lynch <simon.lynch@hashicorp.com>
2024-05-14 10:51:13 +1000

    update

--- variables.tf
+++ variables.tf
@@ -9,7 +9,7 @@ variable "organization" {
 variable "workspace_name" {
   description = "The workspace to read outputs"
   type        = string
-  default     = "datazone_domain4"
+  default     = "retail_payments_datazone_ws"
 }
 
 variable "region" {
@@ -21,11 +21,11 @@ variable "region" {
 variable "datazone_domain_id" {
   description = "Domain ID of datazone; input value is rendered as output in the terraform-awscc-datazone-domain module"
   type = string
-  default = ""
+  default = null
 }
 
 variable "datazone_project_id" {
   description = "Project ID of one of the projects associated to a datazone; input value is rendered as output in the terraform-awscc-datazone-domain module"
   type = string
-  default = ""
+  default = null
 }
